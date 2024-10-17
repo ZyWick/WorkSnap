d414a36b9288661e57ba08369543f6ca5bdbc1a7
kenwin728 <kenwin728@gmail.com>
2024-10-17 18:06:31 +0800

    added layout for the main employee view

--- app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
@@ -17,9 +17,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<DayAttendanceData> DayAttendanceList = new ArrayList<DayAttendanceData>();
-    private DayAttendanceAdapter DayAttendanceAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,21 +28,5 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        RecyclerView recyclerView = findViewById(R.id.dayAttendanceRecycler);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        DayAttendanceList = new ArrayList<DayAttendanceData>();
-        for(int i = 0; i < 20; i++) {
-            ArrayList<AttendancePhotoData> AttendancePhotoDataList = new ArrayList<AttendancePhotoData>();
-            int randomNumPhotos = (int) (Math.random() * 12 + 1);
-            for(int j = 0; j < randomNumPhotos; j++) {
-                AttendancePhotoDataList.add(new AttendancePhotoData());
-            }
-            DayAttendanceList.add(new DayAttendanceData(Timestamp.now(), AttendancePhotoDataList));
-        }
-
-        DayAttendanceAdapter = new DayAttendanceAdapter(DayAttendanceList,MainActivity.this);
-        recyclerView.setAdapter(DayAttendanceAdapter);
     }
 }
