11c40960487586d3a7afbacd192c9e6a1eb4f2b7
kenwin728 <kenwin728@gmail.com>
2024-10-17 18:58:19 +0800

    added "All photos" recycler view

--- app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
@@ -8,15 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.Timestamp;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<MyPhotoData> PhotoDataList;
+    private MyPhotoAdapter MyPhotoAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+        RecyclerView recyclerView = findViewById(R.id.allPhotosRecycler);
+        recyclerView.setHasFixedSize(true);
 
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
+        PhotoDataList = new ArrayList<MyPhotoData>();
+        for(int i = 0; i < 46; i++) {
+            PhotoDataList.add(new MyPhotoData(R.drawable.nav_profile));
+        }
+
+        MyPhotoAdapter = new MyPhotoAdapter(PhotoDataList,MainActivity.this);
+        recyclerView.setAdapter(MyPhotoAdapter);
     }
 }
