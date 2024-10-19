f8761bae8b35f1ec76c5e79e75aa6af126ba287a
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2024-10-19 14:16:18 +0800

    + fragmented

--- app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
@@ -16,43 +16,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    private ArrayList<MyPhotoData> PhotoDataList;
-    private MyPhotoAdapter MyPhotoAdapter;
-    TextView allphotosText;
-    TextView badgesText;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
-            return insets;
-        });
-        allphotosText = findViewById(R.id.allphotosText);
-        badgesText = findViewById(R.id.badgesText);
-        RecyclerView recyclerView = findViewById(R.id.allPhotosRecycler);
-        recyclerView.setHasFixedSize(true);
-
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-
-        PhotoDataList = new ArrayList<MyPhotoData>();
-        for(int i = 0; i < 46; i++) {
-            PhotoDataList.add(new MyPhotoData(R.drawable.user_filled));
-        }
-
-        MyPhotoAdapter = new MyPhotoAdapter(PhotoDataList,MainActivity.this);
-        recyclerView.setAdapter(MyPhotoAdapter);
-    }
-    public void chooseAllPhotos(View v){
-        allphotosText.setTypeface(null, Typeface.BOLD);
-        badgesText.setTypeface(null, Typeface.NORMAL);
-    }
-    public void chooseBadges(View v){
-        allphotosText.setTypeface(null, Typeface.NORMAL);
-        badgesText.setTypeface(null, Typeface.BOLD);
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -82,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             int itemId = item.getItemId();
             if (itemId == R.id.LeaderboardMenu) {
             } else if (itemId == R.id.ProfileMenu) {
+                replaceFragment(new profile());
             } else if (itemId == R.id.allEmployeesMenu) {
                 replaceFragment(new AllEmployee());
             } else if (itemId == R.id.checkEmployeeMenu) {
