52cf74bfa37cb13b005800e4e7941e1fabd3266b
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2024-10-19 16:19:00 +0800

    + connected to profile

--- app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
@@ -1,40 +1,14 @@
 package com.mobdeve.s15.worksnap;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.os.Parcel;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.Timestamp;
 import com.mobdeve.s15.worksnap.databinding.ActivityMainBinding;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGoToCamera;
     ActivityMainBinding binding;
 
     @Override
@@ -61,15 +35,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
-        btnGoToCamera = findViewById(R.id.activity_camera_view_btn_gotocamera);
-
-        btnGoToCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CameraView.class);
-                startActivity(intent);
-            }
-        });
     }
 
     private void replaceFragment (Fragment fragment) {
