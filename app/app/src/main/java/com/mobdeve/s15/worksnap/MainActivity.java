a7d27dca6d791ab0a9e7579e69892e042c649d9f
Joseph <masterslyer10@gmail.com>
2024-10-19 14:38:38 +0800

    Created Camera View

--- app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mobdeve.s15.worksnap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGoToCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnGoToCamera = findViewById(R.id.activity_camera_view_btn_gotocamera);
+
+        btnGoToCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraView.class);
+                startActivity(intent);
+            }
         });
     }
 }
