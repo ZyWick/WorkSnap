41ab35173b70d890659052d0c6e8c98c2dbbcaa6
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2024-11-23 08:25:45 +0800

    + friebase

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
+
+//    id("com.android.application")
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,13 +40,15 @@ android {
 }
 
 dependencies {
-
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.protolite.well.known.types)
     implementation(libs.firebase.common)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/MyFirestoreReferences.java
@@ -0,0 +1,49 @@
+package com.mobdeve.s15.worksnap;
+
+public class MyFirestoreReferences {
+
+    // Collections
+    public static final String
+            BADGES_COLLECTION = "Badges",
+            ATTENDANCE_PHOTOS_COLLECTION = "AttendancePhotos",
+            EMPLOYEES_COLLECTION = "Employees";
+
+    // BadgesData fields
+    public static final String
+            BADGE_ID_FIELD = "badgeID",
+            BADGE_NAME_FIELD = "badgeName",
+            BADGE_IMAGE_FIELD = "badgeImage";
+
+    // AttendancePhotoData fields
+    public static final String
+            IMAGE_ID_FIELD = "imageID",
+            EMPLOYEE_NAME_FIELD = "employeeName",
+            EMPLOYEE_ID_FIELD = "employeeID",
+            ATTENDANCE_IMAGE_FIELD = "attendanceImage",
+            TAKEN_AT_FIELD = "takenAt",
+            LONGITUDE_FIELD = "lon",
+            LATITUDE_FIELD = "lat",
+            VERIFIED_FIELD = "verified",
+            REJECTED_FIELD = "rejected";
+
+    // EmployeeData fields
+    public static final String
+//            Already defined above
+//            EMPLOYEE_ID_FIELD = "employeeID",
+//            EMPLOYEE_NAME_FIELD = "employeeName",
+            EMPLOYEE_IMAGE_FIELD = "employeeImage",
+            TITLE_FIELD = "title",
+            EMAIL_FIELD = "email",
+            WORK_START_FIELD = "work_start",
+            WORK_END_FIELD = "work_end",
+            EMPLOYER_FIELD = "employer",
+            EMPLOYEES_FIELD = "employees",
+            IMAGE_COUNT_TODAY_FIELD = "image_count_today",
+            IMAGE_COUNT_WEEK_FIELD = "image_count_week",
+            IMAGE_COUNT_YEAR_FIELD = "image_count_year",
+            BADGES_FIELD = "badges";
+
+    private MyFirestoreReferences() {
+        // Prevent instantiation
+    }
+}
