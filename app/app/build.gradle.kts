95b3fc4b9e92e758e7d334967cb02b7e12e27043
Hosea Marcus Calalang <hoseagb@gmail.com>
2024-11-27 02:35:42 +0800

    updated build files

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -43,6 +44,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.protolite.well.known.types)
     implementation(libs.firebase.common)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
