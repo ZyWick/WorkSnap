51857e12b501a843a5917792950570f09b8ab15f
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2024-11-27 00:29:47 +0800

    + firbease

--- app/app/src/main/java/com/mobdeve/s15/worksnap/AllEmployee.java
+++ app/app/src/main/java/com/mobdeve/s15/worksnap/AllEmployee.java
@@ -1,13 +1,34 @@
 package com.mobdeve.s15.worksnap;
 
+
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +42,7 @@ public class AllEmployee extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "taggg";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +83,52 @@ public class AllEmployee extends Fragment {
         }
     }
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference usersRef = db.collection(MyFirestoreReferences.USERS_COLLECTION);
+
+    ArrayList<String> employeeIDs = new ArrayList<String>();
+
+
+    public interface OnEmployeeIDsFetchedListener {
+        void onSuccess(ArrayList<String> employeeeeeeeIDs);
+
+        void onFailure(Exception e);
+    }
+
+    public void getEmployeeIDs(String userID, OnEmployeeIDsFetchedListener listener) {
+        DocumentReference docRef = usersRef.document(userID);
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        try {
+                            // Safely retrieve the list of employee IDs
+                            List<String> employees = (List<String>) document.getData().get("employees");
+                            if (employees != null) {
+                                listener.onSuccess(new ArrayList<>(employees));
+                            } else {
+                                Log.d(TAG, "Employees list is null");
+                                listener.onSuccess(new ArrayList<>()); // Return an empty list if field is null
+                            }
+                        } catch (ClassCastException e) {
+                            Log.d(TAG, "Employees field is not of the expected type");
+                            listener.onFailure(e);
+                        }
+                    } else {
+                        Log.d(TAG, "No such document");
+                        listener.onFailure(new Exception("Document does not exist"));
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                    listener.onFailure(task.getException());
+                }
+            }
+        });
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -74,15 +142,62 @@ public class AllEmployee extends Fragment {
         // Set LayoutManager
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
-        // Initialize and populate EmployeeDataList
-        EmployeeDataList = new ArrayList<>();
-        for (int i = 0; i < 46; i++) {
-            EmployeeDataList.add(new EmployeeData("employee " + i));
-        }
+        // Initialize the list of Employee Data
+        ArrayList<EmployeeData> EmployeeDataList = new ArrayList<>();
 
-        // Initialize Adapter and set it to RecyclerView
-        EmployeeAdapter = new EmployeeAdapter(EmployeeDataList, (MainActivity) getActivity());
-        recyclerView.setAdapter(EmployeeAdapter);
+        getEmployeeIDs("T0l1ar5QuLbT8c42wWwp3dTCiay2", new OnEmployeeIDsFetchedListener() {
+            @Override
+            public void onSuccess(ArrayList<String> employeeIDs) {
+                if (!employeeIDs.isEmpty()) {
+                    // Query to fetch employee data based on IDs
+                    Query employeeDataQuery = usersRef.whereIn(FieldPath.documentId(), employeeIDs);
+
+                    employeeDataQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+//                                ArrayList<EmployeeData> EmployeeDataListss = new ArrayList<>();
+//                                for (QueryDocumentSnapshot document : task.getResult()){
+//                                    Log.e(TAG, "Edasdasdas21312dasd" );
+//                                    Log.e(TAG, "Edasdasd" + document);}
+////                                    EmployeeDataListss.add(document.toObject(EmployeeData.class));}
+//
+//                                Log.e(TAG, "Employee Name: " + EmployeeDataListss);
+//                                EmployeeAdapter employeeAdapter = new EmployeeAdapter(EmployeeDataListss, (MainActivity) getActivity());
+//                                recyclerView.setAdapter(employeeAdapter);
+//
+                                QuerySnapshot querySnapshot = task.getResult();
+                                if (querySnapshot != null && !querySnapshot.isEmpty()) {
+
+                                    for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                                        // Safely retrieve the "name" and "position" fields
+                                        String name = document.getString("username");
+                                        String pic = document.getString("profilePhoto");
+
+                                        // Add employee data to the list
+                                        EmployeeDataList.add(new EmployeeData(name, pic, document.getId()));
+                                        Log.e(TAG, "Employee Name: " + name + ", Position: " + document);
+                                    }
+
+                                    // Initialize the adapter after data is fetched
+                                    EmployeeAdapter employeeAdapter = new EmployeeAdapter(EmployeeDataList, (MainActivity) getActivity(), getParentFragmentManager());
+                                    recyclerView.setAdapter(employeeAdapter);
+                                } else {
+                                    Log.d(TAG, "No documents found");
+                                }
+                            } else {
+                                Log.e(TAG, "Error fetching documents", task.getException());
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.e(TAG, "Error fetching employee IDs", e);
+            }
+        });
 
         return view;
     }
